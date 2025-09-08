b74054a9d9ef5689cc1d7487e0d265edc00555a8
Marktuay <marktuay@gmail.com>
2025-09-07 19:12:39 -0600

    correction del archivo vite.config.js, cambio de la ruta de la app

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: 'https://marktuay.github.io/app-react/', // Usa el nombre exacto de tu repositorio aquí
+  base: '/app-react/', // Usa el nombre exacto de tu repositorio aquí
 })
